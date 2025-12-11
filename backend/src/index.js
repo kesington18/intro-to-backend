c8e4b99355941bf809c1cc7e51466cd438eaa010
Kesington Abdulwakeel <mr.abdulwakeel@yahoo.com>
2025-12-11 23:00:47 +0100

    chore: set up database connection and server initialization

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import app from "./app";
+
+
+dotenv.config({
+    path: './.env'
+});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.on("error", (error) => {
+            console.log("ERROR", error)
+
+            throw error;
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on port : ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log("MONGODB connection failed", error);
+    }
+}
+
+startServer(); 
